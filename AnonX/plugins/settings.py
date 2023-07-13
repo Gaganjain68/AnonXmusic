07c34dac28af55171ab7fab556eacc7e379b9e31
Gagan jain <129584007+Gaganjain68@users.noreply.github.com>
2023-07-13 17:45:05 +0530

    Update settings.py

--- AnonX/plugins/settings.py
+++ AnonX/plugins/settings.py
@@ -1,10 +1,16 @@
 from pyrogram import filters
 from pyrogram.errors import MessageNotModified
-from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
-                            InlineKeyboardMarkup, Message)
+from pyrogram.types import (
+     CallbackQuery, 
+     InlineKeyboardButton,
+     InlineKeyboardMarkup,
+     InputMediaPhoto,
+     InputMediaVideo,
+     Message,
+)
 
 from config import (BANNED_USERS, CLEANMODE_DELETE_MINS,
-                    MUSIC_BOT_NAME, OWNER_ID)
+                    MUSIC_BOT_NAME, OWNER_ID,)
 from strings import get_command
 from AnonX import app
 from AnonX.utils.database import (add_nonadmin_chat,
@@ -48,6 +54,17 @@ async def settings_mar(client, message: Message, _):
         reply_markup=InlineKeyboardMarkup(buttons),
     )
 
+
+@app.on_callback_query(filters.regex("gib_source") & ~BANNED_USERS)
+@languageCB
+async def gib_repo(client, CallbackQuery, _):
+    await CallbackQuery.edit_message_media(
+        InputMediaVideo("https://te.legra.ph/file/e6471d19bd04a5095436a.mp4"),
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]
+        ),
+    )
+  
 
 @app.on_callback_query(
     filters.regex("settings_helper") & ~BANNED_USERS

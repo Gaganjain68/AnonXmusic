3c11b30e0d778bc88006b894c4afb7edd4fabd49
Gagan jain <129584007+Gaganjain68@users.noreply.github.com>
2023-07-28 12:31:27 +0530

    Update settings.py

--- AnonX/plugins/settings.py
+++ AnonX/plugins/settings.py
@@ -59,8 +59,7 @@ async def settings_mar(client, message: Message, _):
 @languageCB
 async def gib_repo(client, CallbackQuery, _):
     await CallbackQuery.edit_message_media(
-        InputMediaVideo("https://te.legra.ph/file/e6471d19bd04a5095436a.mp4",
-        has_spoiler=True),
+        InputMediaVideo("https://te.legra.ph/file/e6471d19bd04a5095436a.mp4"),
          
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]

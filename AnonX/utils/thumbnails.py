945f62f8b27c92c8d39defd4d0da92c139911047
Gagan jain <129584007+Gaganjain68@users.noreply.github.com>
2023-08-28 18:53:17 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -95,6 +95,10 @@ async def gen_thumb(videoid, user_id):
 
         Xcenter = youtube.width / 2
         Ycenter = youtube.height / 2
+        x1 = Xcenter - 0
+        y1 = Ycenter - 0
+        x2 = Xcenter + 0
+        y2 = Ycenter + 0
        
         logo = youtube.crop((x1, y1, x2, y2))
         logo.save(f"cache/chop{videoid}.png")

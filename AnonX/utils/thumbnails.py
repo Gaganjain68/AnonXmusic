da0f59bd276838b1fe6cdda62db2a5cf9e2aa0d8
Gagan jain <129584007+Gaganjain68@users.noreply.github.com>
2023-09-24 15:42:30 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -82,7 +82,7 @@ async def gen_thumb(videoid, user_id):
         x = f.resize((150, 150))
 
         youtube = Image.open(f"cache/thumb{videoid}.png")
-        bg = Image.open(f"AnonX/assets/anonx.png")
+        bg = Image.open(f"AnonX/assets/Anonx3.png")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
         background = image2.filter(filter=ImageFilter.BoxBlur(20))
@@ -227,7 +227,7 @@ async def gen_qthumb(videoid, user_id):
         x = f.resize((150, 150))
 
         youtube = Image.open(f"cache/thumb{videoid}.png")
-        bg = Image.open(f"AnonX/assets/anonx.png")
+        bg = Image.open(f"AnonX/assets/Anonx3.png")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
         background = image2.filter(filter=ImageFilter.BoxBlur(10))
